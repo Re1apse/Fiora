cc1bc9843b99738db68e946f95a798eec8c70bf7
Re1apse <seanc987@outlook.com>
2015-03-28 16:25:19 +0000

    Update Program.cs

--- Fiora/Program.cs
+++ Fiora/Program.cs
@@ -249,7 +249,7 @@ namespace Fiora
                 k = 0;
             }
         }
-        public static void Game_OnGameUpdate(EventArgs args)
+        public static void Game.OnUpdate(EventArgs args)
         {
             if (Player.IsDead)
                 return;
